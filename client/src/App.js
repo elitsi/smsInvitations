823ca0da7bd70194280739f6a12c95717e1853c7
Embar Almog <embar.almog@gmail.com>
2019-06-01 23:17:50 +0300

    supprt scss & dish dropdowns

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ import { ArrivalsComponent } from './components'
 function App() {
   return (
     <div className="App">
-      {/* <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" />
+      <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" />
       <script src="https://unpkg.com/react/umd/react.production.js" crossOrigin />
       <script src="https://unpkg.com/react-dom/umd/react-dom.production.js" crossOrigin />
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" /> */}
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
 
       <ArrivalsComponent></ArrivalsComponent>
     </div>
