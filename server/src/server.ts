22d95bdb69d404877388b4fd06368855032e2880
elitsi <elitsinberg@gmail.com>
2019-06-29 17:52:59 +0300

    Running env prepare only if its not production environment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import App from './app'
 import UserController from './controllers/user.controller'
 import IndexController from './controllers/index.controller'
-import { prepareEnvironment } from './utils/config/prepareEnv'
 
-prepareEnvironment();
+if(process.env.environment != 'prod') {
+    const prepareEnvironment = require('./utils/config/prepareEnv').prepareEnvironment;
+    prepareEnvironment();
+}
 
 const app = new App([new UserController(), new IndexController()])
 
